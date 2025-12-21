5d70de98cf2813aa564d199518755bea8047e393
=gala <2718641786@qq.com>
2025-12-21 23:54:43 +0800

    底部导航栏静态布局完成

---
+++ src/utils/vant-ui.js
@@ -0,0 +1,7 @@
+// 引入vant组件
+import Vue from 'vue'
+
+import { Tabbar, TabbarItem } from 'vant'
+
+Vue.use(Tabbar)
+Vue.use(TabbarItem)
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-    Layout
+    <!-- 底部导航 -->
+    <van-tabbar active-color="#ee0a24" inactive-color="#000">
+      <van-tabbar-item icon="wap-home-o">首页</van-tabbar-item>
+      <van-tabbar-item icon="apps-o">分类页</van-tabbar-item>
+      <van-tabbar-item icon="shopping-cart-o">购物车</van-tabbar-item>
+      <van-tabbar-item icon="user-o">我的</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
